5eeb6052922c46f54c40ea1981a9adeefd758121
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2026-06-06 17:52:04 +0545

    fix(config): add path to load env from the root

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,4 +1,7 @@
-import "dotenv/config";
+import path from "node:path";
+import { config } from "dotenv";
+
+config({ path: path.resolve(__dirname, "../../../../.env") });
 
 export const env = {
   NODE_ENV: process.env.NODE_ENV ?? "development",
